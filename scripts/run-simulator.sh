7730a93e9007b921653666424ad6447afaaf3fe8
Mateusz Kedzierski <mateuszkedzierski@vectronic-aerospace.com>
2026-07-02 23:42:49 +0200

    refactor: add SIM_RUNTIME env to simulator run script

--- scripts/run-simulator.sh
+++ scripts/run-simulator.sh
@@ -1,8 +1,10 @@
 cd ../src/simulator
 
 CONFIG_BUCKET=faastreams-config \
-CONFIG_OBJECT=query-config.yaml \
-PUBSUB_PROJECT_ID=faastreams \
-PUBSUB_TOPIC_ID=ais-stream \
-SOURCE_NAME=ais_data_v1 \
+  CONFIG_OBJECT=query-config.yaml \
+  PUBSUB_PROJECT_ID=faastreams \
+  PUBSUB_TOPIC_ID=ais-stream \
+  SOURCE_NAME=ais_data_v1
+# SIM_RUNTIME=5m \ # duration of the simulation in real time
 go run .
+
