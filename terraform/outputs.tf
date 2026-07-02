cc61bbc46239259f6f5fa9aa135c088cd7a2eda5
Salam <salam.zahr10@gmail.com>
2026-07-02 20:51:49 +0200

    typo (stray b)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,7 +8,7 @@ output "worker_url" {
   value       = module.worker.url
 }
 
-output "data_sink_url" {b
+output "data_sink_url" {
   description = "HTTP trigger URL for the data-sink function."
   value       = module.data_sink.url
 }
