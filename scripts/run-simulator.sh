211d254d341cfb24fdcf0ae50dc9f18945b0195b
Mateusz Kedzierski <mateuszkedzierski@vectronic-aerospace.com>
2026-07-06 23:59:41 +0200

    chore: fix deployment scripts & configs

--- scripts/run-simulator.sh
+++ scripts/run-simulator.sh
@@ -4,7 +4,6 @@ CONFIG_BUCKET=faastreams-config \
   CONFIG_OBJECT=query-config.yaml \
   PUBSUB_PROJECT_ID=faastreams \
   PUBSUB_TOPIC_ID=ais-stream \
-  SOURCE_NAME=ais_data_v1
-# SIM_RUNTIME=5m \ # duration of the simulation in real time
-go run .
-
+  SOURCE_NAME=ais_data_v1 \
+  SIM_RUNTIME=5m \
+  go run . # SIM_RUNTIME sets duration of the simulation in real time
